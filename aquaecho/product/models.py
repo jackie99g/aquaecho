78efd1a2ba35e258b3f6e67c9d994260bacd8bf9
jackie99g <jackie99g@gmail.com>
2022-08-21 20:05:51 +0900

    Add AcessLog to models.py.

--- aquaecho/product/models.py
+++ aquaecho/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class AcessLog(models.Model):
+    method = models.CharField(max_length=127)
+    host = models.CharField(max_length=255)
+    full_path_info = models.TextField()
+    scheme = models.CharField(max_length=255)
+    content_type = models.CharField(max_length=255, blank=True)
+    meta_data = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
